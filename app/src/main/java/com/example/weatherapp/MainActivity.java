367aec0c5863b870e494c7c1c143714298cd742b
Niyantha <niyantha23052001@gmail.com>
2020-04-05 17:03:57 +0530

    downloaded JSON response

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 import retrofit2.Call;
@@ -15,42 +21,49 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private TextView test;
+    public class DownloadTask extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result="";
+            URL url;
+            HttpURLConnection urlConnection=null;
+
+            try{
+                url=new URL(urls[0]);
+                urlConnection=(HttpURLConnection)url.openConnection();
+                InputStream input=urlConnection.getInputStream();
+                InputStreamReader reader=new InputStreamReader(input);
+                int data =reader.read();
+                while (data!=-1)
+                {
+                    char current=(char)data;
+                    result+=current;
+                    data=reader.read();
+
+                }
+                return result;
+
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.i("JSON",""+s);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        test=findViewById(R.id.test);
-        Retrofit retrofit=new Retrofit.Builder()
-                .baseUrl("https://api.openweathermap.org/data/2.5/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        WeatherApi weatherApi = retrofit.create(WeatherApi.class);
-        Call<List<Weather>> call =weatherApi.getWeather();
-        call.enqueue(new Callback<List<Weather>>() {
-            @Override
-            public void onResponse(Call<List<Weather>> call, Response<List<Weather>> response) {
-                if(!response.isSuccessful()){
-                    test.setText("code:"+response.code());
-                    return;
-                }
-                List<Weather> weathers=response.body();
-                for(Weather weather:weathers){
-                    String content="";
-                    content+=weather.getmTimeStamp()+"\n";
-                    content+=weather.getmMaxTemp()+"\n";
-                    content+=weather.getmMinTemp()+"\n";
-                    content+=weather.getmDescrip()+"\n\n";
-                    test.append(content);
-                }
+        DownloadTask task=new DownloadTask();
+        task.execute("https://api.openweathermap.org/data/2.5/forecast?q=Chennai&appid=fc7a4df678d008f3db0aa92ea746fa75");
 
-            }
 
-            @Override
-            public void onFailure(Call<List<Weather>> call, Throwable t) {
-                test.setText(t.getMessage());
-
-            }
-        });
-    }
+}
 }
